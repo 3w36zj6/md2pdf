dcce476d4b222bdeba1005a8aa3b4212038b8309
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2021-06-09 20:07:34 +0900

    add header.tex

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR=$(dirname $0)
 cd $(dirname $1)
-pandoc -F pandoc-crossref $(basename $1) -o report.pdf --pdf-engine=lualatex -N
+pandoc -H "${SCRIPT_DIR%/}/header.tex" -F pandoc-crossref $(basename $1) -o report.pdf --pdf-engine=lualatex -N
 cd -
